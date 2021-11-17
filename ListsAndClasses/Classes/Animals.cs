a33d469d4a1ca99686c36cc96c6d608d91831566
Polina0602 <idearlead@gmail.com>
2021-11-17 14:33:56 +0200

    Add project files.

---
+++ ListsAndClasses/Classes/Animals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListsAndClasses.Classes;
+using ListsAndClasses.Interfaces;
+
+
+namespace ListsAndClasses.Classes
+{
+    class Animals : GreenLife, IAnimals
+    {
+    public string AnimalName { get; set; }
+     public  string AnimalType { get; set; }
+
+        public bool AnimalTail;
+
+        public Animals()
+        {
+
+            AnimalName = AnimalName;
+            AnimalType = AnimalType;
+
+        }
+
+        public void RunByPaws()
+        {
+            Console.WriteLine("Top,top,top");
+        }
+
+        //private string PrivateName(string AnimalName)
+        //{
+        //    return AnimalName.ToUpper();
+        //}
+
+
+    }
+
+
+}
---
+++ ListsAndClasses/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using ListsAndClasses.Classes;
+
+namespace ListsAndClasses
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animals PetOfTali = new Animals();
+            PetOfTali.AnimalName = "Joy";
+            PetOfTali.AnimalType = "Dog";
+            PetOfTali.AnimalTail = true;
+            Console.WriteLine($"Tali has a pet. Her name is {PetOfTali.AnimalName}. She is a { PetOfTali.AnimalType}. She has { PetOfTali.Paws} paws.");
+            PetOfTali.RunByPaws();
+            Console.WriteLine();
+
+           
+
+            List<Animals> animals = new List<Animals>();
+            animals.Add(new Animals() { AnimalName = "Tom", AnimalType = "Dog", AnimalTail = true });
+            animals.Add(new Animals() { AnimalName = "Bu", AnimalType = "Cat", AnimalTail = true });
+            animals.Add(new Animals() { AnimalName = "Sharik", AnimalType = "Wolf", AnimalTail = false });
+            animals.Add(new Animals() { AnimalName = "Jack", AnimalType = "Beast", AnimalTail = true });
+
+
+
+            foreach (var animal in animals)
+            {
+                Console.Write(animal.AnimalName);
+                Console.Write("---");
+                Console.Write(animal.AnimalType);
+                Console.Write("     ");
+                Console.Write("Paws");
+                Console.Write(" - ");
+                Console.Write(animal.Paws);
+                Console.Write("     ");
+                IfTale(animal.AnimalTail);
+                Console.WriteLine();
+            }
+        }
+        static void IfTale(bool AnimalTail)
+        {
+
+            if (AnimalTail)
+            {
+                Console.WriteLine("There is a tail!");
+            }
+            else
+            {
+                Console.WriteLine("No tail((");
+            }
+        }
+    }
+
+
+
+
+
+
+}
+
